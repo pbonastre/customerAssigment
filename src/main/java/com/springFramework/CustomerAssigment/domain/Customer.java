73547f57639b072a9a475a8134565db1390c1725
Bonastre Borras <PABE@gft.com>
2020-02-26 12:51:21 +0100

    first commit

---
+++ src/main/java/com/springFramework/CustomerAssigment/domain/Customer.java
@@ -0,0 +1,6 @@
+package com.springFramework.CustomerAssigment.domain;
+
+public class Customer {
+
+
+}
---
+++ src/test/java/com/springFramework/CustomerAssigment/CustomerAssigmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springFramework.CustomerAssigment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerAssigmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
